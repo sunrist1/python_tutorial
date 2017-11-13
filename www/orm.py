a045151c72d7d087b3b811b7f4cbc43c3e970283
sunrist1 <294333575@qq.com>
2017-11-13 23:58:10 +0800

    orm

---
+++ www/orm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+@asyncio.coroutine
+def create_pool(loop,**kw):
+  logging.info('create database connection pool...')
+  global __pool
+  __pool = yield from aiomysql.create_pool(
+    host = kw.get('host','localhost'),
+    port = kw.get('port',3306),
+    user = kw['user'],
+    password = kw['password'],
+    charset = kw.get('charset','utf-8'),
+    db = kw['db'],
+    autocommit = kw.get('autocommit',True),
+    maxsize = kw.get('maxsize',10),
+    minsize = kw.get('minsize',1),
+    loop = loop
+  )
