9f7b6384055e7bf8a6f78cfa3a695c096624c7df
sunrist1 <294333575@qq.com>
2017-11-14 18:26:30 +0800

    'aa'

--- www/orm.py
+++ www/orm.py
@@ -17,3 +17,63 @@ def create_pool(loop,**kw):
     minsize = kw.get('minsize',1),
     loop = loop
   )
+
+
+# 查询select
+@async.coroutine
+def select(sql,args,size=None):
+  log(sql,args)
+  global __pool
+  with(yield from __pool) as conn:
+    cur = yield from conn.cursor(aiomysql.DictCursor)
+    yield from cur.execute(sql.replace('?','%s'),args or ())
+    if size:
+      rs = yield from cur.fetchmany()
+    else:
+      rs = yield from cur.fetchall()
+    yield from cur.close()
+    logging.info('rows returned:%s' % len(rs))
+    return rs
+
+# insert update delete
+@asyncio.coroutine
+def execute(sql,args):
+  log(sql)
+  with (yield from __pool) as conn:
+    try:
+      cur = yield from conn.cursor()
+      yield from cur.execute(sql.replace('?','%s'),args)
+      affected = cur.rowcount
+      yield from cur.close()
+    except BaseException as e:
+      raise
+    return affected
+
+class Model(dict,metaclass=ModelMetaclass):
+
+  def __init__(self,**kw):
+    super(Model,self).__init__(**kw)
+  
+  def __getattr__(self,key):
+    try:
+      return self[key]
+    except KeyError:
+      raise AttributeError(r"'Model' object has no attribute '%s' " %key)
+  
+  def __setattr__(self,key,value):
+    self[key] = value
+  
+  def getValue(self,key):
+    return getattr(self,key,None)
+
+  def getValueOrDefault(self,key):
+    value = getattr(self,key,None)
+    if value is None:
+      field = self.__mappings__[key]
+      if field.default is not None:
+        value = field.default() if callable(field.default) else field.default
+        logging.debug('using default value for %s:%s' % (key,str(value)))
+        setattr(self,key,value)
+
+    return value
+  
